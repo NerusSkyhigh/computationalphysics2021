3a49dfef0079f9d4a8a61eb1c245ded83c741ce5
nerussyhigh <Guglielmo.grillo@gmail.com>
2021-05-09 20:55:34 +0200

    script as modified at the end of our zoom call

--- Exercise_2/Hartree_Fock_GG.py
+++ Exercise_2/Hartree_Fock_GG.py
@@ -5,103 +5,89 @@ from numba import njit
 import scipy.linalg as la
 
 Z = 2 # For Helium
-alphas = np.array([14.899983, 2.726485, 0.757447, 0.251390])
 
-a = 0.001
-delta = 0.001
+alfa = np.array([14.899983, 2.726485, 0.757447, 0.251390] )
+
+a = 0.01
+delta = 0.0001
 
 
+# Computes the single particle, direct and exchange integrals with given orbitals
 @njit
-def OneBodyH(alphas):
-    # Compute the one body terms of the Hamiltonian
-    H_mat = np.zeros([4,4])
-
-    for i in range(0, alphas.shape[0]):
+def integrals(alfa):
+    # SINGLE PARTICLE HAMILTONIAN
+    H_mat = np.zeros((4,4))
+    for i in range(0, 4):
         for j in range(0, i+1):
+            H_mat[i,j] = H_mat[j,i] =2*( 3 * (alfa[i] * alfa[j] * m.pi**(3.0/2.0) ) / \
+                                       (( alfa[i]+ alfa[j])**(5.0/2.0) ) \
+                                     - Z * 2 * m.pi / (alfa[i] + alfa[j]) )
 
-            Kinetic = 3 * (alphas[i] * alphas[j] * m.pi**(3.0/2.0) ) / \
-                         ( (alphas[i]+alphas[j])**(5.0/2.0) )
-
-            Vext = - Z * 2 * m.pi / (alphas[i] + alphas[j])
-
-            H_mat[i,j] = H_mat[j,i] = 2*Kinetic + 2*Vext
-
-    return H_mat
-
-
-
-@njit
-def OverlapMatrix(alphas):
-    # Compute the overlap Matrix
-    S = np.zeros([4,4])
-
-    for i in range(0, alphas.shape[0]):
+    # OVERLAP MATRIX
+    S = np.zeros((4,4))
+    for i in range(0, 4):
         for j in range(0, i+1):
-            S[i,j] = S[j,i] = (m.pi / (alphas[i] + alphas[j] ) )**(3.0/2.0)
+            S[i,j] = S[j,i] = (m.pi / (alfa[i] + alfa[j] ) )**(3.0/2.0)
 
-    return S
-
-
-# Computes the direct and exchange integrals with the given orbitals
-@njit
-def integrals(alphas):
     # 2-BODY POTENTIAL
     Pot_mat = np.zeros((16,16))
-
-    for p in range(0, 4):
-        for q in range(0, p+1):
-            for r in range(0, 4):
-                for s in range(0, r+1):
-
+    for p in range(0, 4, 1):
+        for q in range(0, p+1, 1):
+            for r in range(0, 4, 1):
+                for s in range(0, r+1, 1):
                     if (4*s+r <= 4*q+p):
-                        Pot_mat[4*q+p, 4*s+r] = m.pi**(5.0/2.0) / (4 * alphas[p]
-                        * alphas[q] * alphas[r] * alphas[s]) * (m.sqrt(alphas[p] + alphas[q])
-                        * m.sqrt(alphas[r] + alphas[s]) / m.sqrt(alphas[p] + alphas[q] + alphas[r]
-                        + alphas[s])) * S[p,q] * S[r,s]
+                        # Check pdf "integrals.pdf"
+                        Pot_mat[4*q+p, 4*s+r] = 2/m.sqrt(m.pi) * \
+                            (m.sqrt(alfa[p] + alfa[q]) * m.sqrt(alfa[r] + alfa[s]) / \
+                              m.sqrt(alfa[p] + alfa[q] + alfa[r] + alfa[s])) \
+                              * S[p,q] * S[r,s]
+
+                        # Pot_mat[4*q+p, 4*s+r] = m.pi**(5.0/2.0) / (4 * alfa[p]
+                        #  * alfa[q] * alfa[r] * alfa[s]) * (m.sqrt(alfa[p] + alfa[q])
+                        #  * m.sqrt(alfa[r] + alfa[s]) / m.sqrt(alfa[p] + alfa[q] + alfa[r]
+                        #  + alfa[s])) * S[p,q] * S[r,s]
+
 
 
     # Now we must assign a value to the missing elements in the "upper part"
-    for i in range(0,16):
-        j = 0
-        while j < i:
+    for i in range(0, 16):
+        for j in range(0, i):
             Pot_mat[j,i] = Pot_mat[i,j]
-            j += 1
-
 
     # Now fill the missing rows and columns
-    for q in range (0,4,1):
-        for p in range (q,4,1):
+    for q in range (0, 4):
+        for p in range (q, 4):
             Pot_mat[(4*p +q), :] = Pot_mat[(4*q + p), :]
 
 
-    for q in range (0,4,1):
-        for p in range (q,4,1):
+    for q in range (0,4):
+        for p in range (q, 4):
             Pot_mat[:, (4*p +q)] = Pot_mat[:, (4*q + p)]
 
+    return H_mat, Pot_mat, S
 
-    return Pot_mat
 
 
-# Building the Fock Matrix, representing the Fock operator PROBLEMS
-
-#@njit
+# Building the Fock Matrix, representing the Fock operator
 def fock(H, P, C, C_old):
-
     F = np.zeros((4,4))
 
-    for p in range(0,4,1):
-        for q in range(0,4,1):
+    DensMat = C @ C.transpose()
+    DensMat_old = C_old @ C_old.transpose()
 
+    C = a * DensMat + (1-a)*DensMat_old
+
+    for p in range(0, 4):
+        for q in range(0, 4):
             twob = 0.0
 
-            for r in range(0,4,1):
-                for s in range(0,4,1):
+            for r in range(0, 4):
+                for s in range(0, 4):
+                    P_term = P[4*q + p, 4*s + r] - 0.5*P[4*s +p, 4*q +r]
 
-                    twob += a * C[r] * C[s] * ( P[4*q + p, 4*s + r] - P[4*s +p, 4*q +r] )
-                    + (1 - a) * C_old[r] * C_old[s] * ( P[4*q + p, 4*s + r] - P[4*s +p, 4*q +r] )
+                    twob += C[r, s] * P_term
 
-            F[p,q] = H[p,q] + twob
-
+            F[p,q] = 0.5*H[p,q] + twob
 
     return F
 
@@ -110,20 +96,17 @@ def fock(H, P, C, C_old):
 def diagonalisation(F, S):
 
     # Diagonalise S matrix
-
     eigvalS, eigvecS = la.eig(S)
 
     eigvalS = eigvalS.real
     eigvecS = eigvecS.real
 
     # Sort them
-
     idx = eigvalS.argsort()[::1]
     eigvalS = eigvalS[idx]
     eigvecS = eigvecS[:,idx]
 
     #V MATRIX CONSTRUCTION and TRANSPOSE
-
     V = np.empty([4,4], dtype = float)
 
     for i in range (0, 4):
@@ -147,21 +130,18 @@ def diagonalisation(F, S):
     return eigvalF, Cnew
 
 
-# @njit
-def iteration(H, P, S):
 
+def iteration(H, P, S):
     i = 0
 
     Eoldt = -100
-    Enewt = -1
+    Enewt = -90
+    diff = 100
 
-    while (abs(Enewt - Eoldt) > delta):
+    C = np.zeros([4, 2])
+    C_old = C
 
-        if i == 0 :
-
-            C = np.zeros((4,4))
-            C_old = C
-
+    while (diff > delta):
         Eoldt = Enewt
 
         F = fock(H, P, C, C_old)
@@ -169,23 +149,23 @@ def iteration(H, P, S):
         Enew, Cnew = diagonalisation(F, S)
 
         C_old = C
-
-        C = Cnew
+        C = np.column_stack(( Cnew[:, 0], Cnew[:, 0]))
 
         Enewt = Enew[0]
 
-        print(i)
+        diff = abs(Enewt - Eoldt)
+
+        print(i, Enewt)
 
         i += 1
+        if i == 10000:
+            break
 
 
     return Enew, Cnew, F, C_old
 
 
+
 # Compute matrices with dedicated function
-
-H, P, S = integrals(alphas)
-
-# Set initial values for the vector of the coefficients
-
+H, P, S = integrals(alfa)
 Enew, Cnew, F, C_old = iteration(H, P, S)
