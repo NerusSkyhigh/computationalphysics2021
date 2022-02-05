3c752b3c44dd6726cf77f551cd758b442982087f
siria98 <siriapasini@gmail.com>
2022-02-05 13:42:24 +0100

    Add files via upload

--- Exercise 03/DFT_SP.py
+++ Exercise 03/DFT_SP.py
@@ -5,6 +5,7 @@ import numpy as np
 from numba import njit
 import math as m
 import matplotlib.pyplot as plt
+import sympy as sy
 
 
 # Define system parameters
@@ -40,23 +41,23 @@ b4 = 0.49294
 
 # Mixing parameter
 
-a = 0.1
+a = 0.2
 
 # Define a function that computes the direct term in the potential
 
 @njit 
 def direct(r, rho):
-    rpr1 = np.zeros(Nx)
-    rpr2 = np.zeros(Nx)
+    rho1 = np.zeros(Nx)
+    rho2 = np.zeros(Nx)
     for i in range(0,int(r/dx)):
-        rpr1[i] = dx*i
+        rho1[i] = rho[i]
     for j in range(int(r/dx),Nx):
-        rpr2[j] = dx*j
+        rho2[j] = rho[j]
     
     if r!=0:
-        d = 4*m.pi* (np.trapz(rho * rpr1**2, rpr1)/r + np.trapz(rho * rpr2, rpr2))
+        d = 4*m.pi* (np.trapz(rho1 * x**2, x)/r + np.trapz(rho2 * x, x))
     else:
-        d = 4*m.pi* np.trapz(rho * rpr2, rpr2)
+        d = 4*m.pi* np.trapz(rho2 * x, x)
     
     return d
 
@@ -133,6 +134,23 @@ def rhoconstruct(R,x,l):
     
     return rho
 
+@njit 
+def drawvec(rhomix):
+    
+    V0 = np.zeros(Nx)
+    di = np.zeros(Nx)
+    co = np.zeros(Nx)
+    ex = np.zeros(Nx)
+
+    for i in range(0, Nx):
+        V0[i] = draw_pot(x[i], rhomix, rhomix[i])
+        di[i] = direct(x[i], rhomix)
+        co[i] = -2.0 * A * (1 + a1 * rs) * m.log(1.0 + 1.0 / (2.0 * A * (b1 * rs**(1.0/2.0) + b2 * rs + b3 * rs**(3.0/2.0) + b4 * rs**(p + 1.0) ) ) )
+        ex[i] = -(3/m.pi *rhomix[i])**(1/3)
+        
+    return V0, di, co, ex
+
+
 
 def level(n,l,Ener,lastnow, rho):
     u = np.zeros(Nx)
@@ -166,7 +184,7 @@ def level(n,l,Ener,lastnow, rho):
 # SELF CONSISTENT PROCEDURE 
 
 
-epsilon = 0.1
+epsilon = 0.01
     
 # Set initial densities
     
@@ -177,15 +195,17 @@ rhofinal = np.zeros(Nx)
 Eold = np.zeros((1,2))
 Enew = np.ones((1,2))
 j= 0
-#while (abs(Enew[0,0]-Eold[0,0]) > epsilon) or (abs(Enew[0,1]-Eold[0,1]) > epsilon) :
+Enew[0,0] = - 2.0 * m.pi * rhob * Rc**2+dE #set the starting energy at the bottom of the well
+
+while (abs(Enew[0,0]-Eold[0,0]) > epsilon) or (abs(Enew[0,1]-Eold[0,1]) > epsilon) :
 #while  (all(abs(rhold - rhonew))) > epsilon or (all(abs(rhold - rhonew))) == 0: # or (abs(Enew[1,0]-Eold[1,0]) > epsilon) :
-for j in range(0,4):
+#for j in range(0,2):
+    print("{}".format(j))
     rhomix = rhoinitial
     u = np.zeros(Nx)
-#    Ener = - 2.0 * m.pi * rhob * Rc**2+dE #set the starting energy at the bottom of the well
     E = np.zeros((1,2))
             
-    u1s, E[0,0],lastnow1s = level(1,0, - 2.0 * m.pi * rhob * Rc**2+dE, 1, rhomix)
+    u1s, E[0,0],lastnow1s = level(1,0, Enew[0,0], 1, rhomix)
     u1p, E[0,1],lastnow1p = level(1,1, E[0,0]+dE , 1, rhomix)
     R1s = np.zeros(Nx)
     R1p = np.zeros(Nx)
@@ -237,20 +257,30 @@ for j in range(0,4):
     #rhold = rho
     Eold = Enew
     Enew = E
-    #j +=1
+    print("{},{}".format(E[0,0],E[0,1]))
+    j +=1
+    print("Next cycle")
+    V0plot, diplot, explot, coplot = drawvec(rhomix)
     
+    plt.plot(x[2:Nx], V0plot[2:Nx], linewidth=0.9, label="Total")
+    
+plt.show()
     
 #rhomix = a*rhonew + (1-a) * rhold 
 
-Ir = np.trapz(rhoinitial*x**2*4*m.pi, x )
+Ir = np.trapz(rhomix*x**2*4*m.pi, x )
+rhomix = rhomix/Ir *Ne
 
-# rhonew = rhonew/Ir *8
+rhopolmix = np.zeros(Nx)
 
-xpol = np.zeros(Nx)
 for i in range (int(Rc/dx), int(Nx)):
-    xpol[i] = dx*i 
+    rhopolmix[i] = rhomix[i]
     
-pol = np.trapz(rhoinitial*xpol**2*4*m.pi, xpol)
+N = np.trapz(rhomix*x**2*4*m.pi, x )
+
+deltaN = np.trapz(rhopolmix* x**2* 4*m.pi, x)
+
+alfa = Rc**3*(1+ deltaN/N) /N
 
 # # WF figure
 # fig = plt.figure()
@@ -268,46 +298,29 @@ pol = np.trapz(rhoinitial*xpol**2*4*m.pi, xpol)
 # # Density figure
 
 fig1 = plt.figure()
-plt.plot(x[1:int(L/dx)], rhoinitial[1:int(L/dx)], linewidth=0.9)
+plt.plot(x, rhomix, linewidth=0.9)
 plt.title("Density with {} electrons".format(Ne))
 #plt.axhline(1, xmin = 0, xmax = Rc, color="grey", linestyle="-.", linewidth=0.9)
 #plt.axhline(0, xmin = Rc/14.24, xmax = 1, color="grey", linestyle="-.", linewidth=0.9)
-#plt.axvline(Rc, ymin = 0, ymax = 1, color="grey", linestyle="-.", linewidth=0.9)
+plt.axvline(Rc, ymin = 0, ymax = 1, color="grey", linestyle="-.", linewidth=0.9)
 plt.savefig("rho_20_el.png", dpi=300)
 
 # # Potential
 
+# V0, di, ex, co = drawvec(rhomix)
 
-@njit 
-def drawvec():
-    
-    V0 = np.zeros(Nx)
-    di = np.zeros(Nx)
-    co = np.zeros(Nx)
-    ex = np.zeros(Nx)
-
-    for i in range(0, Nx):
-        V0[i] = draw_pot(x[i], rhoinitial, rhoinitial[i])
-        di[i] = direct(x[i], rhoinitial)
-        co[i] = -2.0 * A * (1 + a1 * rs) * m.log(1.0 + 1.0 / (2.0 * A * (b1 * rs**(1.0/2.0) + b2 * rs + b3 * rs**(3.0/2.0) + b4 * rs**(p + 1.0) ) ) )
-        ex[i] = -(3/m.pi *rhoinitial[i])**(1/3)
-        
-    return V0, di, co, ex
-
-V0, di, ex, co = drawvec()
-
-fig2 = plt.figure()
-plt.plot(x[2:Nx], V0[2:Nx], linewidth=0.9, label="Total")
-plt.axhline(E[0,0] , color="blue", linestyle="-.", label="1s", linewidth=0.9)
-plt.axhline(E[0,1] , color="blue", linestyle="-.", label="2s", linewidth=0.9)
-#plt.axhline(E[1,1] , color="blue", linestyle="-.", label="2p", linewidth=0.9)
-plt.plot(x[2:Nx], di[2:Nx], linewidth=0.9, color="magenta", label="direct")
-plt.plot(x[2:Nx], ex[2:Nx] , linewidth=0.9, color="green", label="exchange")
-plt.plot(x[2:Nx], co[2:Nx] , linewidth=0.9, color="green", label="correlation")
-plt.title("Potential")
-# plt.axhline(En1s , color="green", linestyle="-.", label="1s", linewidth=0.9)
-# plt.axhline(En1p , color="blue", linestyle="-.", label="1p", linewidth=0.9)
-# plt.axhline(En1d , color="red", linestyle="-.", label="1d", linewidth=0.9)
-# plt.axhline(En2s , color="cyan", linestyle="-.", label="2s", linewidth=0.9)
-plt.legend()
-plt.savefig("Pot_20_el.png", dpi=300)
+# fig2 = plt.figure()
+# plt.plot(x[2:Nx], V0[2:Nx], linewidth=0.9, label="Total")
+# plt.axhline(E[0,0] , color="blue", linestyle="-.", label="1s", linewidth=0.9)
+# plt.axhline(E[0,1] , color="blue", linestyle="-.", label="2s", linewidth=0.9)
+# #plt.axhline(E[1,1] , color="blue", linestyle="-.", label="2p", linewidth=0.9)
+# plt.plot(x[2:Nx], di[2:Nx], linewidth=0.9, color="magenta", label="direct")
+# plt.plot(x[2:Nx], ex[2:Nx] , linewidth=0.9, color="green", label="exchange")
+# plt.plot(x[2:Nx], co[2:Nx] , linewidth=0.9, color="red", label="correlation")
+# plt.title("Potential")
+# # plt.axhline(En1s , color="green", linestyle="-.", label="1s", linewidth=0.9)
+# # plt.axhline(En1p , color="blue", linestyle="-.", label="1p", linewidth=0.9)
+# # plt.axhline(En1d , color="red", linestyle="-.", label="1d", linewidth=0.9)
+# # plt.axhline(En2s , color="cyan", linestyle="-.", label="2s", linewidth=0.9)
+# plt.legend()
+# plt.savefig("Pot_20_el.png", dpi=300)
